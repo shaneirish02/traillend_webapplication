aa442cb16a27a411dcda26eccf7e7b32885084de
shaneirish02 <shaneirishyecyec@gmail.com>
2025-11-28 01:17:34 +0800

    apps

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,30 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError, ProgrammingError
+import os
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        # Only create if env vars exist
+        if username and password:
+            try:
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(
+                        username=username,
+                        email=email,
+                        password=password
+                    )
+                    print(f"✔ Superuser '{username}' created automatically.")
+                else:
+                    print(f"✔ Superuser '{username}' already exists.")
+            except (OperationalError, ProgrammingError):
+                # Database not ready (migrations not finished)
+                pass
