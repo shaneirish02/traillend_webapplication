24db9e2b5bd403e2a1683894b077a63adce390b5
shaneirish02 <shaneirishyecyec@gmail.com>
2025-12-01 19:15:00 +0800

    Add scheduler endpoin

--- traillend_final_web/urls.py
+++ traillend_final_web/urls.py
@@ -7,8 +7,11 @@ from core.views import run_smart_scheduler
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include('core.urls')),
     path("", lambda request: redirect("login")),  # redirect root to login page
     path("", include('core.urls')),
     path("api/run-scheduler/", run_smart_scheduler),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
